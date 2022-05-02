3597d32bc10058049bc7c64b1c94fcc0eba9125a
ANGUYEN625 <103291720+ANGUYEN625@users.noreply.github.com>
2022-05-02 00:15:45 -0700

    Pass Test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest{
     @Test
     public void testConnectStr(){
-        assertEquals("forty five", SkillDemo.connectStr("forty", "five"));
+        assertEquals("forty-five", SkillDemo.connectStr("forty", "five"));
     }
 }
